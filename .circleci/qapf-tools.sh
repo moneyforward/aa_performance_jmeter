baa203321232eb4ba6a5ac1cd936769c8c756b50
shevchenki <nguyen.duy.nam@moneyforward.co.jp>
2024-10-07 13:07:44 +0700

    test

--- .circleci/qapf-tools.sh
+++ .circleci/qapf-tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Download qapf"
-asseturl=$(curl -H "Authorization: token ${token}" https://api.github.com/repos/moneyforward/cqoo-qapf/releases/latest | jq -r '.assets[] | select(.name=="qapf.zip") | .url')
-curl -L -H "Authorization: token ${token}" -H "Accept:application/octet-stream" ${asseturl} >qapf.zip
+asseturl=$(curl -H "Authorization: token ${MINAMI_GITHUB_TOKEN}" https://api.github.com/repos/moneyforward/cqoo-qapf/releases/latest | jq -r '.assets[] | select(.name=="qapf.zip") | .url')
+curl -L -H "Authorization: token ${MINAMI_GITHUB_TOKEN}" -H "Accept:application/octet-stream" ${asseturl} >qapf.zip
 unzip qapf.zip
 chmod +x qapf
