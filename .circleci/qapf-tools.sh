15748145053ca2aa70844c0665691fac9e477184
shevchenki <nguyen.duy.nam@moneyforward.co.jp>
2024-10-07 11:22:53 +0700

    test

--- .circleci/qapf-tools.sh
+++ .circleci/qapf-tools.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "Hello, World!"
+echo "Intall GH CLI"
+pip3 install yq
+curl -sSL https://github.com/cli/cli/releases/download/v2.6.0/gh_2.6.0_linux_amd64.tar.gz -o gh_2.6.0_linux_amd64.tar.gz
+tar xvf gh_2.6.0_linux_amd64.tar.gz
+sudo cp gh_2.6.0_linux_amd64/bin/gh /usr/local/bin/
